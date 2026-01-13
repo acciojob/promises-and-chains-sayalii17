5904c36ed14ff6ce053b91a8004645af67f48a6b
Sayali Patil <137858267+sayalii17@users.noreply.github.com>
2026-01-13 19:34:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+let ageIp= document.getElementById("age");
+let nameIp= document.getElementById("name");
+let btn= document.getElementById("btn");
+
+btn.addEventListener("click", (e) => {
+	e.preventDefault();
+	
+	let age= ageIp.value;
+	let name= nameIp.value;
+	
+	if(!age || !name){
+		alert("Please enter valid details.");
+		return
+	}
+
+	const votePromise= new Promise((resolve, reject) => {
+		setTimeout(() => {
+			if(age > 18){
+				resolve(`Welcome, ${name}. You can vote.`);
+			}else{
+				reject(`Oh sorry ${name}. You aren't old enough.`);
+			}
+		}, 3000);
+	});
+
+	
+	votePromise.then((data) => {
+		alert(data);
+	}).catch((err) => {
+		alert(err);
+	});
+});
